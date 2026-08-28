0734e3debc18d7edf5113dc1a2bf2317b66cec2e
Jack Doyle <107753991+J9ck@users.noreply.github.com>
2026-08-28 10:00:27 -0400

    Add files via upload

---
+++ SkillsRI/SkillsRI/SkillsRIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillsRIApp.swift
+//  SkillsRI
+//
+//  Created by Jack Doyle on 8/28/26.
+//
+
+import SwiftUI
+
+@main
+struct SkillsRIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
